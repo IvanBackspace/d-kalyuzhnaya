84d69d4cc27180bdb857385eb0a78c586eb96ad6
Ivan <andreevivanx@email.com>
2026-07-29 13:53:11 +0300

    hh

--- index.php
+++ index.php
@@ -1,238 +1,105 @@
-<div class="popup" data-target="popup-quiz">
-  <div class="popup__body">
-    <form class="popup__content popup-quiz__content">
-      <picture class="close-popup close-popup-btn">
-        <source srcset="/assets/img/icons/popup-close.svg" type="image/svg+xml">
-        <img src="/assets/img/icons/popup-close.png" alt="close_popup" loading="lazy">
-      </picture>
+<style>
+  .what-apart {
+    border-radius: 30px;
+    padding: 70px 145px 10px;
+    background: linear-gradient(154.84deg, #3c8fce 15.73%, #e9f5ff 130.61%);
+  }
 
-      <div class="popup-quiz__pages">
-        <div class="popup-quiz__page" style="display: block;">
-          <p class="popup-quiz__title">Когда планируете размещение в пансионате?</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-1" name="params[message][quiz][]" value="В ближайшее время">
-              <label for="quiz-1">В ближайшее время</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-2" name="params[message][quiz][]" value="Узнаю информацию на будущее">
-              <label for="quiz-2">Узнаю информацию на будущее</label>
-            </div>
-          </div>
-        </div>
+  .what-apart__top {
+    margin-bottom: 30px;
+  }
 
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Какого пола человек, который будет проживать в пансионате?</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sex-1" name="params[message][sex][]" value="Мужчина">
-              <label for="quiz-sex-1">Мужчина</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sex-2" name="params[message][sex][]" value=" Женщина">
-              <label for="quiz-sex-2">Женщина</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sex-3" name="params[message][sex][]" value="Пара, мужчина и женщина">
-              <label for="quiz-sex-3">Пара, мужчина и женщина</label>
-            </div>
-          </div>
-        </div>
+  .what-apart__title {
+    font-family: var(--second-family);
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 120%;
+    text-transform: uppercase;
+    text-align: center;
+    color: var(--belyy-100);
+    margin-bottom: 15px;
+  }
 
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Возраст</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-1" name="params[message][age][]" value="Менее 50 лет">
-              <label for="quiz-age-1">Менее 50 лет</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-2" name="params[message][age][]" value="От 50 до 60 лет">
-              <label for="quiz-age-2">От 50 до 60 лет</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-3" name="params[message][age][]" value="От 60 до 70 дет">
-              <label for="quiz-age-3">От 60 до 70 дет</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-4" name="params[message][age][]" value="От 70 до 80 лет">
-              <label for="quiz-age-4">От 70 до 80 лет</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-5" name="params[message][age][]" value="От 80 до 90 лет">
-              <label for="quiz-age-5">От 80 до 90 лет</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-age-6" name="params[message][age][]" value="Более 90 лет">
-              <label for="quiz-age-6">Более 90 лет</label>
-            </div>
+  .what-apart__subtitle {
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 143%;
+    text-align: center;
+    color: var(--belyy-100);
+  }
 
-          </div>
-        </div>
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Может ли человек передвигаться самостоятельно или ему требуется помощь?</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-help-1" name="params[message][help][]" value="Передвигается самостоятельно">
-              <label for="quiz-help-1">Передвигается самостоятельно</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-help-2" name="params[message][help][]" value="Передвигается с помощью">
-              <label for="quiz-help-2">Передвигается с помощью</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-help-3" name="params[message][help][]" value="Лежачий">
-              <label for="quiz-help-3">Лежачий</label>
-            </div>
-          </div>
-        </div>
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Вес пожилого человека</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-1" name="params[message][weight][]" value="Менее 50 кг">
-              <label for="quiz-weight-1">Менее 50 кг</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-2" name="params[message][weight][]" value="От 50 до 60 кг">
-              <label for="quiz-weight-2">От 50 до 60 кг</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-3" name="params[message][weight][]" value="От 60 до 70 кг">
-              <label for="quiz-weight-3">От 60 до 70 кг</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-4" name="params[message][weight][]" value="От 70 до 80 кг">
-              <label for="quiz-weight-4">От 70 до 80 кг</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-5" name="params[message][weight][]" value="От 80 до 90 кг">
-              <label for="quiz-weight-5">От 80 до 90 кг</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-weight-6" name="params[message][weight][]" value="Более 90 кг">
-              <label for="quiz-weight-6">Более 90 кг</label>
-            </div>
-          </div>
-        </div>
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Выберите все имеющиеся заболевания </p>
-          <div class="popup-quiz__checkboxs">
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-1" name="params[message][]" value="Нет серьёзных заболеваний">
-              <label for="quiz-diseases-1">Нет серьёзных заболеваний</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-2" name="params[message][]" value="Онкология">
-              <label for="quiz-diseases-2">Онкология</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-3" name="params[message][]" value="Перенесен инсульт">
-              <label for="quiz-diseases-3">Перенесен инсульт</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-4" name="params[message][]" value="Альцгеймер">
-              <label for="quiz-diseases-4">Альцгеймер</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-5" name="params[message][]" value="Перенесен инфаркт">
-              <label for="quiz-diseases-5">Перенесен инфаркт</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-6" name="params[message][]" value="Паркинсон">
-              <label for="quiz-diseases-6">Паркинсон</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-7" name="params[message][]" value="Диабет">
-              <label for="quiz-diseases-7">Диабет</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-8" name="params[message][]" value="Деменция">
-              <label for="quiz-diseases-8">Деменция</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-9" name="params[message][]" value="Глаукома">
-              <label for="quiz-diseases-9">Глаукома</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-10" name="params[message][]" value="Язва желудка / кишечника">
-              <label for="quiz-diseases-10">Язва желудка / кишечника</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-11" name="params[message][]" value="Заболевания крови">
-              <label for="quiz-diseases-11">Заболевания крови</label>
-            </div>
-            <div class="sort-checkbox white">
-              <input type="checkbox" id="quiz-diseases-12" name="params[message][]" value="Заболевания кожи, язвы на теле, пролежни">
-              <label for="quiz-diseases-12">Заболевания кожи, язвы на теле, пролежни</label>
-            </div>
-          </div>
-        </div>
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title">Коммуникабельность</p>
-          <div class="popup-quiz__radios">
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sociability-1" name="params[message][sociability][]" value="Общительный">
-              <label for="quiz-sociability-1">Общительный</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sociability-2" name="params[message][sociability][]" value="Раздражительный">
-              <label for="quiz-sociability-2">Раздражительный</label>
-            </div>
-            <div class="input-radio white">
-              <input type="radio" id="quiz-sociability-3" name="params[message][sociability][]" value="Замкнутый">
-              <label for="quiz-sociability-3">Замкнутый</label>
-            </div>
-          </div>
-        </div>
-        <div class="popup-quiz__page" style="display: none;">
-          <p class="popup-quiz__title title-h3 original-title">Вы можете получить скидку на размещение!</p>
-          <div class="popup-quiz__prices">
-            <div class="popup-quiz__price">
-              <p>от 1 500 ₽ / сут.</p>
-              <span>от 1 700 ₽ / сут.</span>
-            </div>
-            <div class="popup-quiz__price">
-              <p>от 32 500 ₽ / мес.</p>
-              <span>от 42 500 ₽ / мес.</span>
-            </div>
-          </div>
-          <p class="popup-quiz__text">Чтобы получить скидку, оставьте ваш номер телефона:</p>
-          <div class="popup-quiz__form">
-            <div class="form-inner">
-              <input name="catalogue" type="hidden" value="1">
-              <input name="BitrixID" type="hidden" value="REPEAT_SALE">
-              <input name="city" type="hidden" value="Москва">
-              <input name="type" type="hidden" value="request">
-              <input name="cc" type="hidden" value="2">
-              <input name="sub" type="hidden" value="8">
-              <input name="mes" type="hidden" value="22">
-              <input name="urlMessage" type="hidden" value="https://yandexwebcache.net/yandbtm">
-              <input name="title_url_message" type="hidden" value="Дома престарелых «Рядом» в Москве и Московской области - частные и государственные интернаты для пожилых">
-              <input type="tel" name="params[phone][]" class="input" placeholder="Номер телефона" required="">
-              <div class="politic">
-                <input type="checkbox" id="politic" name="politic" checked="">
-                <label for="politic">Нажимая кнопку я соглашаюсь <a href="/private-politic/" rel="nofollow">с политикой конфиденциальности</a> и пользовательским соглашением</label>
-              </div>
-              <button class="btn-white" type="submit">Записаться</button>
-              <div style="display: none;"><input name="trafficSource" type="hidden" value="referral">
-                <input name="istochnik" type="hidden" value="ya.ru">
-                <input name="clientID" type="hidden" value="1768291818750256561">
-              </div>
-            </div>
+  .what-apart__rehab {
+    margin-bottom: 10px;
+  }
 
-          </div>
+  .what-apart__clinic,
+  .what-apart__rehab {
+    border-radius: 26px;
+    padding: 40px;
+    background: var(--belyy-100);
+  }
+
+  .what-apart__clinic-title,
+  .what-apart__rehab-title {
+    font-family: var(--second-family);
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 125%;
+    text-transform: uppercase;
+    color: var(--tekst-dark-100);
+    margin-bottom: 18px;
+  }
+
+  .what-apart__rehab-list,
+  .what-apart__clinic-list {
+    border-radius: 20px;
+    padding: 20px 14px;
+    background: var(--fonovyy);
+    padding-left: 20px;
+  }
+
+  .what-apart__rehab-list li,
+  .what-apart__clinic-list li {
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 143%;
+    color: var(--tekst-dark-80);
+  }
+</style>
+<section class="what-apart">
+  <div class="what-apart__container container">
+    <div class="what-apart__wrapper">
+      <div class="what-apart__top">
+        <p class="what-apart__title">Чем мы отличаемся от обычного рехаба</p>
+        <p class="what-apart__subtitle">В большинстве частных центров один зависимый лечит другого по программам из книжек. Мы устроены иначе.</p>
+      </div>
+      <div class="what-apart__bottom">
+        <div class="what-apart__rehab">
+          <p class="what-apart__rehab-title">Обычный рехаб</p>
+          <ul class="what-apart__rehab-list">
+            <li>Консультанты без медицинского образования</li>
+            <li>Чужие программы 12 шагов как единственная основа</li>
+            <li>Нет ежедневного медицинского сопровождения</li>
+            <li>Изолированный центр без медицинской лицензии</li>
+            <li>Работа только с пациентом</li>
+            <li>Выписка без дальнейшей поддержки</li>
+          </ul>
         </div>
-        <div class="popup-quiz__bottom" style="display: flex;">
-          <div class="popup-quiz__btns">
-            <button class="popup-quiz__btn-prev btn-green btn-small" disabled="">Предыдущий вопрос</button>
-            <button class="popup-quiz__btn-next btn-white btn-small" disabled="">Следующий вопрос</button>
-          </div>
-          <div class="popup-quiz__paginations">
-            <span>01</span> / <span>07</span>
-          </div>
+        <div class="what-apart__clinic">
+          <p class="what-apart__clinic-title">Клиника доктора Калюжной</p>
+          <ul class="what-apart__clinic-list">
+            <li>Дипломированные врачи в штате: психиатр-нарколог, невролог, терапевт</li>
+            <li>Авторская программа медико-социальной реабилитации</li>
+            <li>Ежедневные анализы, врачебные обходы, подбор препаратов</li>
+            <li>Медицинское учреждение, лицензия Минздрава № Л041-01126-23/01066181</li>
+            <li>Работа со всей семейной системой — семейные сессии, работа
+              с созависимостью</li>
+            <li>ПЛП — постлечебная программа сопровождения</li>
+          </ul>
         </div>
       </div>
-    </form>
+    </div>
   </div>
-</div>
+</section>
