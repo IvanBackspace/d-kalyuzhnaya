a14705a0ad2533d3243c3fdbe4bf249d8a3f3d20
Ivan <andreevivanx@email.com>
2026-07-30 15:32:47 +0300

    tt

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<section class="spheres-rehabilitation section__general">
-  <div class="spheres-rehabilitation__container container">
-    <div class="spheres-rehabilitation__top section__top">
-      <p class="spheres-rehabilitation__title section__title-general">Государственный диспансер vs клиника КДК
+<section class="plp section__general">
+  <div class="plp__container container">
+    <div class="plp__top section__top">
+      <p class="plp__title section__title-general">
+        ПЛП — <span>сопровождение после выписки</span>
       </p>
-    </div>
-    <div class="spheres-rehabilitation__items">
-      <div class="spheres-rehabilitation__advantages">
-
+      <div class="plp__content">
+        <div>
+          <p class="plp__subtitle section__subtitle-general">Выписка — не конец программы. Первые месяцы после возвращения домой — самый уязвимый период. Наши кураторы остаются рядом.</p>
+        </div>
       </div>
-      <div class="spheres-rehabilitation__functionals">
-        <div class="spheres-rehabilitation__functional">
+    </div>
+    <div class="plp__main">
+      <div class="plp__items">
+        <div class="plp__item">
+          <p class="plp__item-num">
+            01
+          </p>
+          <div class="plp__item-bottom">
 
+          </div>
         </div>
       </div>
     </div>
